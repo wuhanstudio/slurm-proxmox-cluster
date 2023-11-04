57b40a6c89110b60bdb9fbf886cae88a41595c65
Han Wu <wuhanstudio@qq.com>
2023-11-04 23:01:25 +0000

    Update hello-world-submit.sh

--- 2-slurm-scheduler/hello-world-submit.sh
+++ 2-slurm-scheduler/hello-world-submit.sh
@@ -5,8 +5,8 @@
 #SBATCH --nodes=1 # specify number of nodes.
 #SBATCH --ntasks-per-node=1 # specify number of processors per node
 #SBATCH --mail-type=END # send email at job completion
-#SBATCH --output=hello-world.o
-#SBATCH --error=hello-world.e
+#SBATCH --output=/mnt/slurmfs/hello-world.o
+#SBATCH --error=/mnt/slurmfs/hello-world.e
 #SBATCH --job-name=hello-world
 
 ## print start date and time
